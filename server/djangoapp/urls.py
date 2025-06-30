e2e811c272138cd2225498df507446685ca4e8e4
magdalena-kr <magdalena.krexner@gmail.com>
2025-06-30 08:19:48 -0400

    reg

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,9 +8,11 @@ from .views import logout_request
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
-
+    path(route='register', view=views.register, name='register'),
     # path for login
     path(route='login', view=views.login_user, name='login'),
+   
+    # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
