dfe1bf6bb8179d175af15fb0868d9d7ffb9eb2a1
magdalena-kr <magdalena.krexner@gmail.com>
2025-07-01 14:33:07 -0400

    19

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -29,7 +29,10 @@ urlpatterns = [
     path('register/', TemplateView.as_view(template_name="index.html")),
     path('dealers/', TemplateView.as_view(template_name="index.html")),
     path(
-        'dealer/<int:dealer_id>', TemplateView.as_view(template_name="index.html")
+        'dealer/<int:dealer_id>',
+        TemplateView.as_view(
+            template_name="index.html"
+        ),
     ),
     path(
         'postreview/<int:dealer_id>',
